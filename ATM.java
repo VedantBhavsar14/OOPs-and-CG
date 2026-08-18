389ed98165881bfea7964a22916bface135cc9c2
VedantBhavsar14 <vedantbhavsar2006@gmail.com>
2026-08-18 12:47:06 +0530

    Add files via upload

--- ATM.java
+++ ATM.java
@@ -1,5 +1,50 @@
 package Javaproject;
+import java.util.Scanner;
 
-public class ATM {
+class ATM {
+	private double balance;
+	public ATM(double initialBalance) {
+		this.balance=initialBalance;
+	}
+	public void checkBalance() {
+		System.out.println("Current Balance:Rs"+balance);
+	}
+	public void deposit(double amount) {
+		if(amount<=0) {
+			throw new IllegalArgumentException("Deposit must be positive");
+		}
+		balance+=amount;
+		System.out.println("Amount Deposited Successfully"+amount);
+	}
+	public void withdraw(double amount) {
+		if(amount<=0) {
+			throw new IllegalArgumentException("Withdraw should be positive");
+		}
+		if(amount>balance) {
+			throw new ArithmeticException("Insufficient balance");
+		}
+		balance-=amount;
+		System.out.println("Amount Withdraw Successfully"+amount);
+	}
+}
 
+class ATMSimulation{
+	public static void main(String[]args) {
+		Scanner sc=new Scanner(System.in);
+		ATM atm=new ATM(100000);
+		boolean running=true;
+	while (running) {
+		try {
+			System.out.println("1.Check balance");
+			System.out.println("2.Deposit");
+			System.out.println("3.Withdraw");
+			System.out.println("4.Exit");
+			System.out.println("Enter your choice");
+		int choice=sc.nextInt();
+		switch(choice) {
+		
+		}
+			}
+		}
+	}
 }
